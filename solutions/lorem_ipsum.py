bfc29567ee41972e544bb6a2c00820c91cf93df0
Xavier Olive <git@xoolive.org>
2018-09-05 09:22:56 +0200

    correction

--- solutions/lorem_ipsum.py
+++ solutions/lorem_ipsum.py
@@ -3,13 +3,14 @@ import string
 with open("data/00-lorem-ipsum.txt") as f:
     d = {}
     for x in f.readlines():
-        for s in string.punctuation: # !"#$%&\'()*+,-./:;<=>?@[\\]^_‘{|}~ x = x.replace(s, " ")
-            for p in x.split():
-                if p in d.keys():
-                    d[p] += 1
-                else:
-                    d[p] = 1
+        for s in string.punctuation:  # !"#$%&\'()*+,-./:;<=>?@[\\]^_‘{|}~
+            x = x.replace(s, " ")
+        for p in x.split():
+            if p in d.keys():
+                d[p] += 1
+            else:
+                d[p] = 1
 
     for key in sorted(d.keys()):
-        if d[key] > 150: # make the output a bit lighter...
-            print ("{}: {}".format(key, d[key]))
+        if d[key] > 5:  # make the output a bit lighter...
+            print("{}: {}".format(key, d[key]))
