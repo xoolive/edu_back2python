88089204a7825e32d2c47885b91e4710b3ffa4b8
lbasora <41791151+lbasora@users.noreply.github.com>
2019-09-05 15:16:08 +0200

    Correction bug dans la solution trace_sin.py (#3)
    * corrections mineures pour les parties 01-04
    * bug correction

--- solutions/trace_sin.py
+++ solutions/trace_sin.py
@@ -1,5 +1,5 @@
-theta = np.linspace(0.0, 2*np.pi, 50)
-r = np.sin(5*theta)
-x, y = np.cos(r), np.sin(r)
+theta = np.linspace(0.0, 2*np.pi, 100)
+r = np.sin(5 * theta)
+x, y = r * np.cos(theta), r * np.sin(theta)
 fig, ax = plt.subplots(figsize=(10,10))
 plt.plot(x, y)
